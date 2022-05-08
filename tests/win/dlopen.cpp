47080d59c5cc42c776d10ff0e927e0ae1377b57c
Sergey Vidyuk <sir.vestnik@gmail.com>
2022-05-08 23:44:59 +0700

    Fix windows build

--- tests/win/dlopen.cpp
+++ tests/win/dlopen.cpp
@@ -4,7 +4,7 @@
 
 void dlcloser::operator()(void* handle) const noexcept {
   if (handle)
-    FreeLibrary(handle);
+    FreeLibrary(static_cast<HMODULE>(handle));
 }
 dyn_lib dlopen(const std::filesystem::path &path) {
   return dyn_lib{LoadLibrary(path.c_str())};
